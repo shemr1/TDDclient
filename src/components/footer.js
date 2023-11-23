388dc4fdb4b8a248e5e4bc0e2de39665fa7db549
SBW <shemar.brown-wright@digicelgroup.com>
2023-11-22 20:18:18 -0500

    #2

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,22 +10,22 @@ export default function Footer(){
 			</div>
 			<ul className="flex flex-wrap items-center space-x-4 sm:space-x-8">
 				<li>
-					<a rel="noopener noreferrer" href="#">Terms of Use</a>
+					<a rel="noopener noreferrer" href="/">Terms of Use</a>
 				</li>
 				<li>
-					<a rel="noopener noreferrer" href="#">Privacy</a>
+					<a rel="noopener noreferrer" href="/">Privacy</a>
 				</li>
 			</ul>
 		</div>
 		<ul className="flex flex-wrap pl-3 space-x-4 sm:space-x-8">
 			<li>
-				<a rel="noopener noreferrer" href="#">Instagram</a>
+				<a rel="noopener noreferrer" href="/">Instagram</a>
 			</li>
 			<li>
-				<a rel="noopener noreferrer" href="#">Facebook</a>
+				<a rel="noopener noreferrer" href="/">Facebook</a>
 			</li>
 			<li>
-				<a rel="noopener noreferrer" href="#">Twitter</a>
+				<a rel="noopener noreferrer" href="/">Twitter</a>
 			</li>
 		</ul>
 	</div>
